a099b7b537e58a909c15b14b40a447358d10d0d8
Maxasi <ma.si@bluemail.ch>
2017-11-27 11:15:43 +0100

    Fetching JSON-objects now working

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,24 +1,14 @@
 function loadPizzas() {
-
-
-
-
     var xhttp = new XMLHttpRequest();
-
-    var url = "https://tonyspizzafactory.herokuapp.com/api/pizzas?authorization=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.MQ.bYceSpllpyYQixgNzDt7dpCkEojdv3NKD-85XLXfdI4";
-    //authorization = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.MQ.bYceSpllpyYQixgNzDt7dpCkEojdv3NKD-85XLXfdI4";
-    // password = "...";
-    xhttp.open("GET", url, true);
-
-    //var url = curl "https://tonyspizzafactory.herokuapp.com/api/pizzas -H Authorization: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.MQ.bYceSpllpyYQixgNzDt7dpCkEojdv3NKD-85XLXfdI4";
-    //xhttp.open("GET", url, true);
+    xhttp.open("GET", "https://tonyspizzafactory.herokuapp.com/api/pizzas", true);
+    xhttp.setRequestHeader("Authorization", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.MQ.bYceSpllpyYQixgNzDt7dpCkEojdv3NKD-85XLXfdI4");
+    xhttp.send();
     xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            var myArr = JSON.parse(this.responseText);
-            console.log("drin");
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            //var myArr = xhttp.responseText;
+            console.log(xhttp.responseText);
         }
     };
-    xhttp.send();
-    console.log(xhttp);
-    console.log(JSON.parse.xhttp);
+    console.log(xhttp.responseText);
+    //console.log(myArr);
 }
